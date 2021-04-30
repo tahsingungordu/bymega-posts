43f6fb164cb774b34d23eb46820502d070f8933e
bymega <tahsingungordu@gmail.com>
2021-05-01 01:05:00 +0300

    add config

--- src/PostServiceProvider.php
+++ src/PostServiceProvider.php
@@ -11,10 +11,18 @@ class PostServiceProvider extends ServiceProvider
     $this->app->bind('calculator', function($app) {
         return new Calculator();
     });
+
+    $this->mergeConfigFrom(__DIR__.'/config/config.php', 'bymegaposts');
   }
 
   public function boot()
   {
-    //
+    if ($this->app->runningInConsole()) {
+
+      $this->publishes([
+        __DIR__.'/config/config.php' => config_path('bymegaposts.php'),
+      ], 'config');
+
+    }
   }
 }
